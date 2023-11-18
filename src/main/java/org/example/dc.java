05de8352306660c943b6fb1e765c672665c5106f
yu5uf47 <yusufalas103@gmail.com>
2023-11-18 18:58:57 -0500

    first commit

--- src/main/java/org/example/dc.java
+++ src/main/java/org/example/dc.java
@@ -3,43 +3,77 @@ package org.example;
 import java.util.Scanner;
 
 public class dc {
+
+    static Scanner scanner = new Scanner(System.in);
    public static void main(String[] args) {
-         Scanner giris = new Scanner(System.in);
-         int sayi1,sayi2;
-         int dogrusayisi = 0;
-         int yanlissayisi = 0;
-         int score = 0;
-         char tekraroyna = 'e';
-         int dogrucevap=0;
 
-         do {
 
-            sayi1=(int)(Math.random()* 100);
-            sayi2=(int)(Math.random()* 100);
-            dogrucevap=sayi1+sayi2;
 
-            System.out.println("İki sayının toplamı nedir?");
-            System.out.println(sayi1 + " + " + sayi2 + " = ");
-            int cevap = giris.nextInt();
+           System.out.print("Kullanıcı Adını Giriniz: ");
+           String kullaniciAdi = scanner.nextLine(); // kullanıcı adı = taksimetre
+           System.out.print("Şifre Giriniz: ");
+           String sifre = scanner.next(); // şifre = 123
 
-            if (cevap == dogrucevap) {
-               System.out.println("Tebrikler dogru cevap");
-               score += 5;
-               dogrusayisi++;
-            } else {
-               System.out.println("Üzgünüm yanlıs cevap");
-               score -= 2;
-               yanlissayisi++;
-            }
+           if (girisKontrol(kullaniciAdi, sifre)) {
+               menu();
+           } else {
+               System.out.println("Kullanıcı adı veya şifre yanlış!");
+           }
+           scanner.close();
+       }
 
-            System.out.println("Tekrar oynamak istiyor musunuz? (e/E)");
-            tekraroyna = giris.next().charAt(0);
+       public static boolean girisKontrol(String _kullaniciAdi, String _sifre) {
+           boolean result = _kullaniciAdi.equalsIgnoreCase("taksimetre") && _sifre.equals("123");
+           return result;
+       }
 
-         } while (tekraroyna == 'e' || tekraroyna == 'E');
+       public static double taksimetreHesapla(double km) {
+           int acilisUcreti = 10;
+           int mesafeBasi = 100;
+           double mesafeUcreti = 0.65;
+           double ucret = (km * 1000) / mesafeBasi * mesafeUcreti + acilisUcreti;
+           return ucret;
+       }
 
-         System.out.println("Dogru cevap sayısı: " + dogrusayisi);
-         System.out.println("Yanlıs cevap sayısı: " + yanlissayisi);
-         System.out.println("Toplam puanınız: " + score);
-      }
+       public static void menu() {
+           double[] ilcelerArasiMesafe = { 66.6, 35.5, 38.7, 40.7, 9.8, 28.3 };
+           String[] hareketMerkezleri = { "Akkışla", "Bünyan", "Develi", "Felahiye", "Hacılar", "İncesu" };
+           int i = 0;
+           for (String hareketMerkezi : hareketMerkezleri) {
+               System.out.println((i + 1) + ") " + hareketMerkezi + " -> " + ilcelerArasiMesafe[i] + " km");
+               i++;
+           }
+           System.out.print("Gideceğiniz yeri seçiniz: ");
+           int secim = scanner.nextInt();
+           double tutar = 0.0;
+           switch (secim) {
+               case 1:
+                   tutar = taksimetreHesapla(ilcelerArasiMesafe[0]);
+                   System.out.printf("Taksimetre Tutarı : %.2f TL", tutar);
+                   break;
+               case 2:
+                   tutar = taksimetreHesapla(ilcelerArasiMesafe[1]);
+                   System.out.printf("Taksimetre Tutarı : %.2f TL", tutar);
+                   break;
+               case 3:
+                   tutar = taksimetreHesapla(ilcelerArasiMesafe[2]);
+                   System.out.printf("Taksimetre Tutarı : %.2f TL", tutar);
+                   break;
+               case 4:
+                   tutar = taksimetreHesapla(ilcelerArasiMesafe[3]);
+                   System.out.printf("Taksimetre Tutarı : %.2f TL", tutar);
+                   break;
+               case 5:
+                   tutar = taksimetreHesapla(ilcelerArasiMesafe[4]);
+                   System.out.printf("Taksimetre Tutarı : %.2f TL", tutar);
+                   break;
+               case 6:
+                   tutar = taksimetreHesapla(ilcelerArasiMesafe[5]);
+                   System.out.printf("Taksimetre Tutarı : %.2f TL", tutar);
+                   break;
+               default:
+                   System.out.println("Yanlış aralıkta bir seçim yaptınız. Lütfen 1-6 arasında seçim yapınız.");
+           }
+
+       }
    }
-
